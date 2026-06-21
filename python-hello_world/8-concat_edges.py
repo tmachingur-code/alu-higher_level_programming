d4bd1fed544a088fe12c972728b31c9a5cbe3b6e
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-06-21 01:16:51 -0700

    Fix string slicing to match expected output for concat edges task

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 str = "object-oriented programming with Python"
-str2 = str[:6]
-str3 = str[9:18]
-str4 = str[18:29]
-print(str2 + str3 + str4)
+print(str[:6] + str[9:18] + str[18:])
