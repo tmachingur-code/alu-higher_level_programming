1878368e9c8a87c968252dce330d50c810b1b911
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-02 12:27:21 -0700

    Task 5: Remove all 'c' and 'C' characters from a string

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    """Return a new string without the characters 'c' and 'C'."""
+    new_string = ""
+
+    for char in my_string:
+        if char != "c" and char != "C":
+            new_string += char
+
+    return new_string
