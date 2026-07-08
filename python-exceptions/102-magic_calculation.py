10d494bdb03886b23ec2e72ecd048f64944b3d92
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-08 07:20:04 -0700

    feat: implement magic_calculation function from bytecode

---
+++ python-exceptions/102-magic_calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    """Replicates the given Python bytecode behavior."""
+    result = 0
+
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            result += (a ** b) / i
+        except Exception:
+            result = a + b
+            break
+
+    return result
