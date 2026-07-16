84bc5221381be482400532eef056ca92afbbc658
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-16 09:37:33 -0700

    feat: Add area calculation method to Square class

---
+++ python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """Represents a square with a validated size."""
+
+    def __init__(self, size=0):
+        """Initialize a square.
+
+        Args:
+            size: The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than zero.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Return the current square area.
+
+        Returns:
+            The area of the square.
+        """
+        return self.__size ** 2
