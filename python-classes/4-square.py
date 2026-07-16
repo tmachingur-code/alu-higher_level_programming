375d0badace87bfb3fdb4e33c912aa85429e20d4
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-16 09:42:37 -0700

    feat: Add size getter and setter with validation to Square class

---
+++ python-classes/4-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """Represents a square with size validation."""
+
+    def __init__(self, size=0):
+        """Initialize a square.
+
+        Args:
+            size: The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than zero.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the current size of the square.
+
+        Returns:
+            The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set and validate the size of the square.
+
+        Args:
+            value: The new size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than zero.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """Return the current square area.
+
+        Returns:
+            The area of the square.
+        """
+        return self.__size ** 2
