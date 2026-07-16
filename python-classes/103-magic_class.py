12c80c95ff0cc7d7320c391cbeca9742654e9343
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-16 10:20:16 -0700

    feat:Implement MagicClass circle calculations from bytecode

---
+++ python-classes/103-magic_class.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""This module defines the MagicClass class."""
+
+
+import math
+
+
+class MagicClass:
+    """Represents a circle using a radius."""
+
+    def __init__(self, radius=0):
+        """Initialize a MagicClass instance.
+
+        Args:
+            radius: The radius of the circle.
+
+        Raises:
+            TypeError: If radius is not an integer or float.
+        """
+        self.__radius = 0
+
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+
+        self.__radius = radius
+
+    def area(self):
+        """Calculate and return the area of the circle.
+
+        Returns:
+            The area of the circle.
+        """
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """Calculate and return the circumference of the circle.
+
+        Returns:
+            The circumference of the circle.
+        """
+        return 2 * math.pi * self.__radius
