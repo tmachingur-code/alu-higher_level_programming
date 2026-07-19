25c3fd2c82121d263fb6826f5aae7b38eeb3f4f9
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-19 08:03:41 -0700

    Add deletion detection to Rectangle class

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Defines a rectangle with width and height."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a Rectangle."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the perimeter of the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return the rectangle using the # character."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        rectangle = []
+        for _ in range(self.__height):
+            rectangle.append("#" * self.__width)
+
+        return "\n".join(rectangle)
+
+    def __repr__(self):
+        """Return a string representation for eval()."""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Print a message when a Rectangle instance is deleted."""
+        print("Bye rectangle...")
