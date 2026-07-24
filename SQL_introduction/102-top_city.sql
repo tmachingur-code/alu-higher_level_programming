37aaa2fa2a62fc1c84558e048fa5e4f9da54b015
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-24 14:11:50 -0700

    Complete task 19: Get top cities by summer temperature

---
+++ SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- Displays the top 3 cities with the highest average temperature in July and August
+-- Calculate average temperature and order by highest average
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month IN (7, 8)
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
