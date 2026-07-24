c05d60dd1526ab720a942de73ce59f846a006c31
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-24 13:58:29 -0700

    Update database and table charset to utf8mb4

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,17 +1,10 @@
--- Converts hbtn_0c_0 database, table and field to UTF8
--- Set database, table and field character set to utf8mb4
+-- Converts hbtn_0c_0 database and first_table to UTF8
+-- Change charset and collation to utf8mb4_unicode_ci
 
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
-USE hbtn_0c_0;
-
-ALTER TABLE first_table
+ALTER TABLE hbtn_0c_0.first_table
 CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE first_table
-MODIFY name VARCHAR(256)
-CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
