acff6e0f2d9e99c798db664b33cdd69a963d3a31
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-23 23:22:10 -0700

    Implement write_file function using with statement

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,10 @@
 
 
 def write_file(filename="", text=""):
-    """Write a string to a UTF-8 text file and return the number of characters written."""
+    """
+    Write a string to a UTF-8 text file.
+
+    Return the number of characters written.
+    """
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
