0ff60b79a7987aecc898b5139a363ca4a25cc176
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-24 14:03:27 -0700

    Complete task 18: Calculate average temperature by city

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,6 @@
--- Displays average temperature by city
--- Calculate average temperature and order by highest average
-SELECT city, AVG(temperature) AS avg_temp
+-- Displays the average temperature by city
+-- Calculate average values and order by highest average
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
