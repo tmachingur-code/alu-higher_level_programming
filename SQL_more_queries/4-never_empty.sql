3f45a722c162fc0a8f84450c7e971851c13130bd
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-25 02:22:21 -0700

    Create id_not_null table with default id value

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,7 @@
+-- Creates id_not_null table with a default value for id
+
+-- Create id_not_null table if it does not exist
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+);
