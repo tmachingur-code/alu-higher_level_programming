52d65a94cb7955392d4d09510e85cc4f74833621
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-25 02:09:01 -0700

    Create read-only MySQL user for hbtn_0d_2 database

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- Creates database hbtn_0d_2 and user user_0d_2 with SELECT privilege
+
+-- Create database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant only SELECT privilege on the database
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
