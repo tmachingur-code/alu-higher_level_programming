d846d6cdb9ea801e65e461c664c82afc26f10065
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-25 02:57:52 -0700

    Create states table with auto increment primary key

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,10 +1,14 @@
--- Creates the hbtn_0d_usa database and states table
+-- Creates hbtn_0d_usa database and states table
 
 -- Create database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+-- Use the database
+USE hbtn_0d_usa;
+
 -- Create states table if it does not exist
 CREATE TABLE IF NOT EXISTS states (
-    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
 );
