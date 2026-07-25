b7cd777cd1c685cbf4b1174420271b764dfcbb7d
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-25 03:52:20 -0700

    Rank TV genres by total ratings

---
+++ SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,10 @@
+-- Lists all genres ordered by their total ratings
+
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings
+ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.name
+ORDER BY rating DESC;
