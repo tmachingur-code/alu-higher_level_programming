7b39339578955e561a849d6a7380f57b4910b43d
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-25 02:17:26 -0700

    Create MySQL user with all privileges

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- Creates MySQL user user_0d_1 with all privileges
+
+-- Create user if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
