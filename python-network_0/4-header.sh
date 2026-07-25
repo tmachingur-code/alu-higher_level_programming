65139b5eadc6775159097f1ff83b58b76aeeab45
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-25 09:34:58 -0700

    Implement 4-header.sh script

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with a custom header and displays the response body.
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Script that sends a GET request to URL and displays body of response
+curl -sH "X-School-User-Id: 98" "$1"
