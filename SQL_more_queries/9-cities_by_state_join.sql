fd7f692edd88764bdb389a1f390f15488cdb03df
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-25 03:06:09 -0700

    List cities with their corresponding states using join

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Lists all cities with their corresponding states
+
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;
