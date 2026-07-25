91e43f99c14c8304eea2b71a126b55d1ff9432be
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-25 02:05:05 -0700

    Update the script

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,34 +1,4 @@
+-- 0-privileges.sql
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on localhost
--- Handles the cases where either or both users may not exist
-DELIMITER $$
-
-CREATE PROCEDURE show_privileges_0d()
-BEGIN
-    DECLARE user1_exists INT;
-    DECLARE user2_exists INT;
-
-    SELECT COUNT(*) INTO user1_exists
-        FROM mysql.user
-        WHERE User = 'user_0d_1' AND Host = 'localhost';
-
-    SELECT COUNT(*) INTO user2_exists
-        FROM mysql.user
-        WHERE User = 'user_0d_2' AND Host = 'localhost';
-
-    IF user1_exists = 0 AND user2_exists = 0 THEN
-        SELECT "Users don't exist";
-    ELSEIF user1_exists = 1 AND user2_exists = 0 THEN
-        SHOW GRANTS FOR 'user_0d_1'@'localhost';
-    ELSEIF user1_exists = 0 AND user2_exists = 1 THEN
-        SHOW GRANTS FOR 'user_0d_2'@'localhost';
-    ELSE
-        SHOW GRANTS FOR 'user_0d_1'@'localhost';
-        SHOW GRANTS FOR 'user_0d_2'@'localhost';
-    END IF;
-END$$
-
-DELIMITER ;
-
-CALL show_privileges_0d();
-
-DROP PROCEDURE show_privileges_0d;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
