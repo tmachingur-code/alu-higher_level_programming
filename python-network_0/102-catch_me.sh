13efd7b0552351f4268fb42262a257f3d3167201
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-25 10:03:35 -0700

    Implement catch_me curl script with redirect handling

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a request to catch_me endpoint using PUT method.
-curl -s -X PUT 0.0.0.0:5000/catch_me
+# Sends a PUT request and follows redirects to get the final response.
+curl -sL -X PUT 0.0.0.0:5000/catch_me
