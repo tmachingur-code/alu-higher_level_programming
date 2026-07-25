d668f27a1c1077174dcb3149ba63c486fcf6b97d
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-25 02:12:35 -0700

    Create force_name table with required name field

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- Creates the force_name table with a non-null name field
+
+-- Create force_name table if it does not exist
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
