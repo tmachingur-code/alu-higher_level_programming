0ba5c2795b16384f90fd8579ac99080a5bdc25d5
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-26 01:47:28 -0700

    Implement 102-catch_me.sh script

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Makes a request that causes the server to respond with "You got me!"
-curl -sL -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
+# Sends a request that satisfies the catch_me endpoint requirements.
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
