b207b01a3a9a87dcf1b80858eea703a9898c1d14
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-26 01:44:25 -0700

    Add Bash script to send request to catch_me endpoint

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a PUT request and follows redirects to get the final response.
-curl -sL -X PUT 0.0.0.0:5000/catch_me
+# Makes a request that causes the server to respond with "You got me!"
+curl -sL -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
