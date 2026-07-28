d9a6c97941c79dba5e982afbeff558f37cf7dc15
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-28 00:49:41 -0700

    Implement POST email request with requests

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Sends a POST request with an email parameter and displays the response."""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    data = {"email": sys.argv[2]}
+
+    response = requests.post(sys.argv[1], data=data)
+
+    print(response.text)
