b0141870dad150d1fa2162df692eac0a568de643
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-27 22:43:26 -0700

    Add urllib status fetch script

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Fetches a URL using urllib and displays the response body."""
+
+from urllib import request
+
+
+if __name__ == "__main__":
+    with request.urlopen("https://alu-intranet.hbtn.io/status") as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
