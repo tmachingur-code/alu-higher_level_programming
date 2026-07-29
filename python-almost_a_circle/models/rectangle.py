b331d34aa5a909b00862e60f22ccbc30f8b5717b
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-29 03:40:50 -0700

    Add update method using args to Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -45,6 +45,25 @@ class Rectangle(Base):
             self.height
         )
 
+    def update(self, *args):
+        """
+        Update rectangle attributes using positional arguments.
+
+        Args:
+            *args: Values assigned in order:
+                id, width, height, x, y.
+        """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
+
     @property
     def width(self):
         """
