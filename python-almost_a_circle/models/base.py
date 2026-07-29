dbe8f87f3653b21e7bb851cde34cab2bca0a953e
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-29 04:23:33 -0700

    Add JSON string serialization method to Base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,3 +20,15 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Return the JSON string representation of a list of dictionaries.
+        """
+        import json
+
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+
+        return json.dumps(list_dictionaries)
