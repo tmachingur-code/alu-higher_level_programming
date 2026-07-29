febc6a8aa0b13fd901eae9f90553aa57fcdd6078
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-29 05:09:38 -0700

    Add JSON string deserialization method to Base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -52,3 +52,15 @@ class Base:
 
         with open(filename, "w") as file:
             file.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Return the list represented by a JSON string.
+        """
+        import json
+
+        if json_string is None or json_string == "":
+            return []
+
+        return json.loads(json_string)
