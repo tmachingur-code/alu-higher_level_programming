aaa939991b851dcbb84f9b10bcfbca75cdadffce
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-31 12:09:36 -0700

    Add script to fetch all states using SQLAlchemy

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Lists all State objects from a MySQL database using SQLAlchemy."""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    session = Session(engine)
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
