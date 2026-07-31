e44f1b3f5ebb3d23dba19266a3ee556cb99d3f7e
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-07-31 12:19:20 -0700

    Add script to fetch first state using SQLAlchemy

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Prints the first State object from a MySQL database using SQLAlchemy."""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    session = Session(engine)
+
+    state = session.query(State).order_by(State.id).first()
+
+    if state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
